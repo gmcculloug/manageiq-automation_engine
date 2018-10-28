caaec1e9f8c055517418976444ff4c9b2101df65
Greg McCullough <gmccullo@redhat.com>
2018-10-28 18:10:16 -0400

    Remove "association => true" from exposed methods.

--- lib/miq_automation_engine/service_models/miq_ae_service_service_template_transformation_plan_request.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_service_template_transformation_plan_request.rb
@@ -1,8 +1,8 @@
 module MiqAeMethodService
   class MiqAeServiceServiceTemplateTransformationPlanRequest < MiqAeServiceServiceTemplateProvisionRequest
-    expose :source_vms, :association => true
+    expose :source_vms
     expose :validate_conversion_hosts
-    expose :validate_vm, :association => true
-    expose :approve_vm, :association => true
+    expose :validate_vm
+    expose :approve_vm
   end
 end
